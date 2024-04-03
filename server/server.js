ae795be61a6417c479a911aeed019d9d02c97ec7
Gabriel <142362789+Gabriel-WestMec@users.noreply.github.com>
2024-04-03 00:27:32 +0000

    added stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const cors = require('cors');
 const app = express();
+// app.use(cors);
 const port = 3000;
 
-app.get('/', (req, res) => { 
-    res.send('hello world')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    // res.send('hello world')
+});
+app.get('csv-data', (req, res) => {
+    const csvPath = path.join(__dirame, 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (eer, data) => {
+        if (eer) {
+            console.error(err);
+            return;
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+})
+app.get('/3pm', (req, res) => {
+    res.send('hello 3pm');
 })
 
 app.listen(port, () => {
